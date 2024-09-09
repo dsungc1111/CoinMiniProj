d6b7825e1de4875c2b41fbd4f0c5795c2b818466
dsungc1111 <eotjd9808@naver.com>
2024-09-09 17:16:02 +0900

    Initial Commit

---
+++ CoinMiniProj/CoinMiniProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinMiniProjApp.swift
+//  CoinMiniProj
+//
+//  Created by 최대성 on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinMiniProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
